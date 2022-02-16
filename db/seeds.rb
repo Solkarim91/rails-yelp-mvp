2a72b0a0828d583b6a8193b6289ca4618b70e471
Solomon Karim <solkarimlive@hotmail.co.uk>
2022-02-16 12:20:32 +0000

    complete the migration & seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do
+  puts 'creating restaurant'
+  restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone_in_e164,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  puts "created restaurant #{restaurant.id}"
+end
